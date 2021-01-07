72a64602358e4ccd9b0d55406eacd8dac08d07e5
yanchen01 <ychen01@bu.edu>
2021-01-07 00:08:55 -0500

    added init and json methods for user model

--- models/user.py
+++ models/user.py
@@ -7,3 +7,18 @@ class User(me.Document):
     email = me.StringField(required=True, unique=True)
     password_hash = me.StringField(required=True)
     age = me.IntField()
+
+    def __init__(self, username, name, email, password, age):
+        self.username = username
+        self.name = name
+        self.email = email
+        self.password_hash = password
+        self.age = age
+
+    def json(self):
+        return {
+            'username': self.username,
+            'name': self.name,
+            'email': self.email,
+            'age': self.age
+        }

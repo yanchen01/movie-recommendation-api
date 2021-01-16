4c4928d9fb7148e89c6fa193e8719582a80c4be1
yanchen01 <ychen01@bu.edu>
2021-01-16 16:04:27 -0500

    created db model for movie

---
+++ models/movie.py
@@ -0,0 +1,12 @@
+import mongoengine as me
+
+
+class Movie(me.Document):
+    name = me.StringField(required=True, unique=True)
+    genre = me.ListField(me.StringField(required=True))
+
+    def json(self):
+        return {
+            'name': self.name,
+            'genre': self.genre
+        }
